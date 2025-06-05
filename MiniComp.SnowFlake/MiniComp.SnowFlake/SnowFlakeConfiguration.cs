2777fbc1a219d0d876cedc527ff514a034d12193
huangzicheng <770233343@qq.com>
2025-06-05 18:15:01 +0800

    init

---
+++ MiniComp.SnowFlake/MiniComp.SnowFlake/SnowFlakeConfiguration.cs
@@ -0,0 +1,64 @@
+﻿namespace MiniComp.SnowFlake;
+
+public static class SnowFlakeConfiguration
+{
+    /// <summary>
+    /// 默认值6，限制每毫秒生成的ID个数。若生成速度超过5万个/秒，建议加大 SeqBitLength 到 10
+    /// </summary>
+    internal static byte SeqBitLength;
+
+    /// <summary>
+    /// 默认值6，限定 WorkerId 最大值为2^6-1，即默认最多支持64个节点
+    /// </summary>
+    internal static byte WorkerIdBitLength;
+
+    /// <summary>
+    /// 基础时间
+    /// </summary>
+    internal static DateTime BaseTime;
+
+    /// <summary>
+    ///工作ID缓存KEY
+    /// </summary>
+    internal static string WorkIdKey = string.Empty;
+
+    /// <summary>
+    /// 占用更新间隔
+    /// </summary>
+    internal static long OccupyUpdateInterval;
+
+    /// <summary>
+    /// 占用过期时间
+    /// </summary>
+    internal static long OccupyExpireTime;
+
+    /// <summary>
+    /// 是否已设置BitLength
+    /// </summary>
+    private static bool _isSet;
+
+    /// <summary>
+    /// WorkId
+    /// </summary>
+    internal static ushort WorkId = 0;
+
+    public static void SetOption(
+        byte seqBitLength = 6,
+        byte workerIdBitLength = 6,
+        DateTime baseTime = new(),
+        long occupyUpdateInterval = 60000,
+        long occupyExpireTime = 60000 + 10000
+    )
+    {
+        if (_isSet)
+        {
+            throw new Exception("请勿重复设置");
+        }
+        _isSet = true;
+        SeqBitLength = seqBitLength;
+        WorkerIdBitLength = workerIdBitLength;
+        BaseTime = baseTime;
+        OccupyUpdateInterval = occupyUpdateInterval;
+        OccupyExpireTime = occupyExpireTime;
+    }
+}
---
+++ MiniComp.SnowFlake/MiniComp.SnowFlake/SnowFlakeHostService.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using MiniComp.Cache;
+using Yitter.IdGenerator;
+
+namespace MiniComp.SnowFlake;
+
+public class SnowFlakeHostService(ILogger<SnowFlakeHostService> logger, ICacheService cacheService)
+    : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        logger.LogInformation("开始分配WorkId");
+        var workMax = (long)Math.Pow(2, SnowFlakeConfiguration.WorkerIdBitLength) - 1;
+        const string incrementKey = "Snowflake:WorkId:IncrementKey";
+        const string lockKey = "Snowflake:WorkId:LockKey";
+        const string occupyKey = "Snowflake:WorkId:";
+        await cacheService.LockAsync(
+            lockKey,
+            async () =>
+            {
+                var isReset = false;
+                while (true)
+                {
+                    var value = await cacheService.GetCacheAsync<ushort?>(incrementKey);
+                    if (!value.HasValue)
+                    {
+                        value = 0;
+                    }
+                    else
+                    {
+                        value += 1;
+                    }
+                    if (value >= workMax)
+                    {
+                        if (isReset)
+                        {
+                            throw new Exception("WorkId已被占满");
+                        }
+                        value = 0;
+                        isReset = true;
+                    }
+                    await cacheService.AddCacheAsync(incrementKey, value.Value);
+                    SnowFlakeConfiguration.WorkIdKey = occupyKey + value.Value;
+                    if (await cacheService.IsExistAsync(SnowFlakeConfiguration.WorkIdKey))
+                        continue;
+                    await cacheService.AddCacheAsync(
+                        SnowFlakeConfiguration.WorkIdKey,
+                        0,
+                        TimeSpan.FromMilliseconds(SnowFlakeConfiguration.OccupyExpireTime)
+                    );
+                    SnowFlakeConfiguration.WorkId = value.Value;
+                    break;
+                }
+            }
+        );
+        YitIdHelper.SetIdGenerator(
+            new IdGeneratorOptions()
+            {
+                SeqBitLength = SnowFlakeConfiguration.SeqBitLength,
+                WorkerIdBitLength = SnowFlakeConfiguration.WorkerIdBitLength,
+                BaseTime = SnowFlakeConfiguration.BaseTime,
+                WorkerId = SnowFlakeConfiguration.WorkId,
+            }
+        );
+        logger.LogInformation("WorkId = {WorkId}", SnowFlakeConfiguration.WorkId);
+        using var periodicTimer = new PeriodicTimer(
+            TimeSpan.FromMilliseconds(SnowFlakeConfiguration.OccupyUpdateInterval)
+        );
+        while (await periodicTimer.WaitForNextTickAsync(stoppingToken))
+        {
+            await cacheService.KeyExpireAsync(
+                SnowFlakeConfiguration.WorkIdKey,
+                TimeSpan.FromMilliseconds(SnowFlakeConfiguration.OccupyExpireTime)
+            );
+        }
+    }
+}
